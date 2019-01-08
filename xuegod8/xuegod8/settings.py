4e4f7ee3fd465f7a6892ee08d64fa6d5d4b84677
1286211699 <1286211699@qq.com>
2019-01-08 11:39:58 +0800

    Djangoajax登陆注册新增

--- xuegod8/xuegod8/settings.py
+++ xuegod8/xuegod8/settings.py
@@ -106,3 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(os.path.join(BASE_DIR, 'static')),
+]
